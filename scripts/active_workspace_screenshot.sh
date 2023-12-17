214f756e1876a1881c71ca924f43677fe0d5f8d0
ipg0 <pyromancy00@gmail.com>
2023-12-17 04:58:31 +0300

    update screenshot functionality & tweak swaync config

--- scripts/active_workspace_screenshot.sh
+++ scripts/active_workspace_screenshot.sh
@@ -3,7 +3,7 @@
 FILE=$(xdg-user-dir PICTURES)/Screenshots/$(date +'Screenshot-%Y-%m-%d-%H%M%S.png')
 BUFFER=~/.tmp/slurp.buffer
 
-slurp -c 9d37fcff > $BUFFER
+slurp -c 00000000 -o -f "%o" > $BUFFER
 # so slurp's selection border has time to disappear before screenshot is taken
 sleep 1
 cat $BUFFER | grim -g - $FILE
